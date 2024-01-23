a66f4326d68855be32c3bad8cc2337a61cb5b2a2
baezluise <107555064+baezluise@users.noreply.github.com>
2024-01-23 12:54:50 -0500

    trabajo hecho en clase 2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+//variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("dame un numero porfa:");
+let numeroUsuario = prompt("dame un numero entre 1 y 10 porfa:");
 
 console.log(numeroUsuario);
 
+console.log("Resultado de la comparación:", numeroUsuario == numeroSecreto);
+
+/* este codigo hace la comparacion
+ */
+
 if (numeroSecreto == numeroUsuario) {
-  alert("acertaste");
+  //acerto
+  alert(`acertaste, el numero secreto es ${numeroSecreto}`);
+} else {
+  //no acerto
+  alert("lo siento, no acertaste");
 }
