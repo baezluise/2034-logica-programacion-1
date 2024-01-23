562e974da36097522d262aea353c129872f1acee
baezluise <107555064+baezluise@users.noreply.github.com>
2024-01-23 01:50:13 -0500

    clase 2 concatenacion

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-alert("Hola Mundo");
+let numeroSecreto = 6;
 let numeroUsuario = prompt("dame un numero porfa:");
+
 console.log(numeroUsuario);
+
+if (numeroSecreto == numeroUsuario) {
+  alert("acertaste");
+}
