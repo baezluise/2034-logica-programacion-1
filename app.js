b361de6a13457db39ffa933101a55cecd650e80a
baezluise <107555064+baezluise@users.noreply.github.com>
2024-01-24 16:06:09 -0500

    clase 3

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 //variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("dame un numero entre 1 y 10 porfa:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraIntentos = "intento";
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("dame un numero entre 1 y 10 porfa:");
 
-console.log(numeroUsuario);
+  console.log(numeroUsuario);
 
-console.log("Resultado de la comparación:", numeroUsuario == numeroSecreto);
+  console.log("Resultado de la comparación:", numeroUsuario == numeroSecreto);
 
-/* este codigo hace la comparacion
- */
-
-if (numeroSecreto == numeroUsuario) {
-  //acerto
-  alert(`acertaste, el numero secreto es ${numeroSecreto}`);
-} else {
-  //no acerto
-  alert("lo siento, no acertaste");
+  // este codigo hace la comparacion
+  if (numeroSecreto == numeroUsuario) {
+    //acerto
+    alert(
+      `acertaste, el numero secreto es ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraIntentos}`
+    );
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("el numero secreto es menor");
+    } else {
+      alert("el numero secreto es mayor");
+    }
+    intentos = intentos + 1;
+    palabraIntentos = "intentos";
+  }
 }
