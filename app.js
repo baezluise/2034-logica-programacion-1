109f9cfc45200fd03a58a69394994f3acad99991
baezluise <107555064+baezluise@users.noreply.github.com>
2024-01-24 16:54:37 -0500

    clase 4

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 //variables
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraIntentos = "intento";
+//let palabraIntentos = "intento";
+let maximosIntentos = 3;
 while (numeroUsuario != numeroSecreto) {
-  numeroUsuario = prompt("dame un numero entre 1 y 10 porfa:");
+  numeroUsuario = parseInt(prompt("dame un numero entre 1 y 10 porfa:"));
 
   console.log(numeroUsuario);
 
   console.log("Resultado de la comparación:", numeroUsuario == numeroSecreto);
 
   // este codigo hace la comparacion
-  if (numeroSecreto == numeroUsuario) {
+  if (numeroSecreto === numeroUsuario) {
     //acerto
     alert(
-      `acertaste, el numero secreto es ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraIntentos}`
+      `acertaste, el numero secreto es ${numeroSecreto}. Lo hiciste en ${intentos} ${
+        intentos == 1 ? "intento" : "intentos"
+      }`
     );
   } else {
     if (numeroUsuario > numeroSecreto) {
@@ -22,7 +25,11 @@ while (numeroUsuario != numeroSecreto) {
     } else {
       alert("el numero secreto es mayor");
     }
-    intentos = intentos + 1;
-    palabraIntentos = "intentos";
+    intentos++;
+    // palabraIntentos = "intentos";
+    if (intentos > maximosIntentos) {
+      alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`);
+      break;
+    }
   }
 }
